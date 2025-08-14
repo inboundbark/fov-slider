b4c60aea61fc3c535e5a8914a5c0b06688662cba
inboundbark <inboundbark49@gmail.com>
2025-08-14 18:02:21 +0200

    remove stapi-dependent code
    will probably need to hardcode ui text

---
+++ src/main/java/io/github/inboundbark/fovslider/mixin/OptionInvoker.java
@@ -0,0 +1,16 @@
+package io.github.inboundbark.fovslider.mixin;
+
+import net.minecraft.client.option.Option;
+import net.modificationstation.stationapi.api.util.Util;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Option.class)
+public interface OptionInvoker {
+    // Yes, this is duplicated code from StAPI.
+    // I'd rather have this than needing an entire dependency for only one method.
+    @Invoker("<init>")
+    static Option fovslider_invokeInitOption(String optionEnumName, int enumOrdinal, String key, boolean slider, boolean toggle) {
+        return Util.assertMixin();
+    }
+}
